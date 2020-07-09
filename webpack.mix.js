d4fea499153dfd73cc304257ce5339d6ffcb50bd
Tara Matthew <taramatthew.93@gmail.com>
2020-07-09 22:11:42 +0100

    Experimenting with laravel-spotify - Adding browser-sync for automatic page refresh

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,8 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
+mix.browserSync('http://localhost:8000/');
+
 mix.webpackConfig({
     resolve: {
         extensions: ['.js', '.vue'],
