568e22d49a925a0b0c649afc0b81c11796f1b6d2
Tara Matthew <taramatthew.93@gmail.com>
2020-09-08 09:15:22 +0100

    Reformat code

--- app/Http/Helpers/Pdf.php
+++ app/Http/Helpers/Pdf.php
@@ -4,7 +4,8 @@ namespace App\Http\Helpers;
 
 use TCPDF;
 
-class Pdf {
+class Pdf
+{
 
     protected $tcpdf;
 
@@ -13,47 +14,55 @@ class Pdf {
         $this->tcpdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
     }
 
-    public function export($info) {
-
+    public function export($info)
+    {
         $this->tcpdf->setCreator(PDF_CREATOR);
         $this->tcpdf->setPrintHeader(false);
         $this->tcpdf->setPrintFooter(false);
         $this->tcpdf->AddPage();
 
-        $html = '<style>
-                    .margin {
-                        margin-bottom:1000px;
-                    }
-                    </style>';
-
-        $html .= '<h1 class="margin" style="text-align:center;">' . $info['info']['playlistTitle'] . '</h1> <br>';
+        $html = '<h1 class="margin" style="text-align:center;">' . $info['info']['playlistTitle'] . '</h1> <br>';
 
         $this->tcpdf->writeHTML($html, true, false, true, false, '');
 
-        foreach($info['tracks'] as $key => $tracks) {
+        foreach ($info['tracks'] as $key => $tracks) {
 
-            $decodedImage = preg_replace('#^data:image/[^;]+;base64,#', '', $tracks['image']);
-            $filteredData = substr($tracks['image'], strpos($tracks['image'], ",")+1);
+            $filteredData = substr($tracks['image'], strpos($tracks['image'], ",") + 1);
             $unencodedData = base64_decode($filteredData);
 
 
-            $html =
-                    '<div class="center">
-                        <p>' . $tracks["name"] . '</p>
-                        <p>' . $tracks['artist'] . '</p>
-                    </div>
-                    <br>';
+            $html = '
+                <div class="center">
+                    <p>' . $tracks["name"] . '</p>
+                    <p>' . $tracks['artist'] . '</p>
+                </div>
+                <br>
+            ';
 
-            $this->tcpdf->Image('@'.$unencodedData,'','','','','','','',false,300,'R',false,false,0,'','',false,false);
+            $this->tcpdf->Image('@' . $unencodedData,
+                '',
+                '',
+                '',
+                '',
+                '',
+                '',
+                '',
+                false,
+                300,
+                'R',
+                false,
+                false,
+                0,
+                '',
+                '',
+                false,
+                false);
 
             $this->tcpdf->writeHTML($html, true, false, true, false, '');
 
         }
 
-
-        $this->tcpdf->Output('C:/Users/taram/Documents/Coding/Laravel/spot-me/exports/'. $info['info']['playlistTitle'] . '.pdf', 'F');
-
-
+        $this->tcpdf->Output('C:/Users/taram/Documents/Coding/Laravel/spot-me/exports/' . $info['info']['playlistTitle'] . '.pdf', 'F');
 
     }
 
