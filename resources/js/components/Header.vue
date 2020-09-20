c461a69ae5889ef3334d262043476bf6a344c7b9
Tara Matthew <taramatthew.93@gmail.com>
2020-09-20 19:58:29 +0100

    Add link on Spot Me in header

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,7 +1,13 @@
 <template>
     <v-toolbar color="#7a2ec7">
         <v-toolbar-title class="white--text">
+            <router-link
+                    to="/playlists"
+                    style="cursor:pointer"
+                    tag="span"
+            >
             Spot Me
+            </router-link>
         </v-toolbar-title>
     </v-toolbar>
 </template>
