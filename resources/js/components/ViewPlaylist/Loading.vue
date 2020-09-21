a7ebe66b20fce59e20204091f1d92b22ba96f8d6
Tara Matthew <taramatthew.93@gmail.com>
2020-09-21 11:08:03 +0100

    Add loading animation

--- resources/js/components/ViewPlaylist/Loading.vue
+++ resources/js/components/ViewPlaylist/Loading.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container v-if="!loadImages">
-        <h1>Loading...</h1>
+        <h1 class="loading">Loading</h1>
     </v-container>
 </template>
 
@@ -12,3 +12,38 @@
         ]
     }
 </script>
+
+<style scoped>
+    .loading:after {
+        content: '.';
+        animation: dots 1s steps(5, end) infinite;}
+
+    @keyframes dots {
+        0%, 20% {
+            color: rgba(0,0,0,0);
+            text-shadow:
+                    .25em 0 0 rgba(0,0,0,0),
+                    .5em 0 0 rgba(0,0,0,0);
+        }
+
+        40% {
+            color: white;
+            text-shadow:
+                    .25em 0 0 rgba(0,0,0,0),
+                    .5em 0 0 rgba(0,0,0,0);
+        }
+
+        60% {
+            text-shadow:
+                    .25em 0 0 white,
+                    .5em 0 0 rgba(0,0,0,0);
+        }
+
+        80%, 100% {
+            text-shadow:
+                    .25em 0 0 white,
+                    .5em 0 0 white;
+        }
+    }
+
+</style>
